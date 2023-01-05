453872f09c913b1efdd7241e4d8605b70073c38c
Zuzana Skubenova <zuzana.skb.ads@gmail.com>
2023-01-05 21:03:37 +0100

    ver 0.5

--- src/GameAdaptor.java
+++ src/GameAdaptor.java
@@ -1,5 +1,60 @@
-public class GameAdaptor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
 
+public class GameAdaptor implements GamePlayerInterface {
     // converts between internal and external types
     // calls correct object to perform each task
+
+    private Game game;
+    private GameObservable gameObservable;
+    private Map<String, Integer> players; // name of player and his index in game
+
+    public GameAdaptor(GameObservable gO, Map<String, Integer> players) {
+        gameObservable = gO;
+        this.players = players;
+        game = new Game(players.size());
+    }
+
+    @Override
+    public String play(String player, String cards) {
+        if (players.size() < 2) {
+            return "There is not enough players.";
+        }
+        if (!players.containsKey(player)) {
+            return "There is no such player: " + player + " in this game.";
+        }
+        int indexOfPlayer = players.get(player);
+        List<Position> c = new ArrayList<>();
+        Scanner scanner = new Scanner(cards);
+        while (scanner.hasNext()) {
+            String string = scanner.next();
+            char first = string.charAt(0);
+
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int i = 1; i < string.length(); i++) {
+                arrayList.add(Integer.parseInt(String.valueOf(string.charAt(i))));
+            }
+
+            if (first == 'h') {
+                c.add(new HandPosition(arrayList.get(0), indexOfPlayer));
+            } else if (first == 'a') {
+                c.add(new AwokenQueenPosition(arrayList.get(1), arrayList.get(0)));
+            } else if (first == 's') {
+                c.add(new SleepingQueenPosition(Integer.parseInt(string.substring(1))));
+            } else {
+                System.out.println("Not correct input.");
+            }
+        }
+        scanner.close();
+        Optional<GameState> gameS = game.play(indexOfPlayer, c);
+        if (gameS.isEmpty()) {
+            return "Error!";
+        }
+        gameObservable.notifyAll(gameS.get());
+        return "Cards were played succesfully.";
+
+    }
 }
--- src/GameObservable.java
+++ src/GameObservable.java
@@ -1,23 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class GameObservable {
 
     // observer pattern implementation
     // convertation between internal and external types
     // hides what should not be seen
 
-    public void add(GameObserverInterface observer) {
+    private List<GameObserverInterface> observers;
+    private Map<Integer, GameObserverInterface> playersObservers;
 
+    public GameObservable() {
+        observers = new ArrayList<>();
+        playersObservers = new TreeMap<>();
+
+    }
+
+    public void add(GameObserverInterface observer) {
+        observers.add(observer);
     }
 
     public void addPlayer(int playerIdx, GameObserverInterface observer) {
-
+        if (playersObservers.size() == 5) {
+            System.out.println("There are already enough players in game.");
+            return;
+        } else if (playerIdx > 0 && playerIdx < 5) {
+            observers.add(observer);
+            playersObservers.put(playerIdx, observer);
+        } else {
+            System.out.println("Index of player has to be between 0 and 4.");
+        }
     }
 
     public void remove(GameObserverInterface observer) {
-
+        if (observers.contains(observer)) {
+            observers.remove(observer);
+        }
     }
 
     public void notifyAll(GameState message) {
+        String m = "Game state:\n";
+        m += "number of players: " + message.numberOfPlayers + "\n";
+        m += "on turn: " + message.onTurn + "\n";
+        m += "cardsDiscartedLastTurn: " + message.cardsDiscardedLastTurn.toString() + "\n";
+        m += "sleepingQueens: " + message.sleepingQueens.toString() + "\n";
+        m += "awokenQueens: " + message.awokenQueens.toString() + "\n";
 
+        for (GameObserverInterface observer : observers) {
+            observer.notify(m);
+        }
+    }
+
+    public List<GameObserverInterface> getObservers() {
+        return observers;
+    }
+
+    public Map<Integer, GameObserverInterface> getPlayersObservers() {
+        return playersObservers;
     }
 
 }
